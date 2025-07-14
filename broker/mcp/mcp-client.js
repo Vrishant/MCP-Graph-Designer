76d4427b39ac97089a0d7cb14bde79931c0883d8
Vrishant <vrishant2bhalla@gmail.com>
2025-07-14 12:33:39 +0530

    API key set

--- broker/mcp/mcp-client.js
+++ broker/mcp/mcp-client.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv");
 const process = require("process");
 
 dotenv.config();
-const OPENAI_API_KEY = "process.env.OPENAI_API_KEY";
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 if (!OPENAI_API_KEY) {
   throw new Error("OPENAI_API_KEY is not set");
 }
